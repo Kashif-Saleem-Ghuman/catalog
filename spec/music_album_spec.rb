fc8fd16d3eae26aa4648c6d671b3999d028e3c72
Christian Ani <iamchristianani@gmail.com>
2023-03-21 15:59:02 +0100

    Add test for initialize methods in both Genre and MusicAlbum class

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe MusicAlbum do
+  describe '#initialize' do
+    it 'creates a new music album with a publish date and on_spotify attribute' do
+      publish_date = '2002-01-01'
+      on_spotify = true
+      album = MusicAlbum.new(publish_date, on_spotify)
+      expect(album.publish_date).to eq(publish_date)
+      expect(album.on_spotify).to eq(on_spotify)
+    end
+  end
+
   describe '#can_be_archived?' do
     it 'returns true if super returns true and @on_spotify is true' do
       album = MusicAlbum.new('2002-01-01', true)
