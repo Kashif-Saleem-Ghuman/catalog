b4cf60af385813c7c48417adeb5a4d552ccc89c7
Ellon M <mordecaimwema11@gmail.com>
2023-03-21 16:22:45 +0300

    Indicated the attributes in the Author class as accessor attributes

--- ruby/game.rb
+++ ruby/game.rb
@@ -1,6 +1,8 @@
 require_relative 'item'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
