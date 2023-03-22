632f76371a96394b7dcdf725aef8b2950e0370b6
unknown <kashif_saleem_23@hotmail.com>
2023-03-22 18:31:17 +0500

    add tables for book and label

--- db/shema.sql
+++ db/shema.sql
@@ -48,7 +48,23 @@ create TABLE music_album (
 
 
 --------------------------kashif starts here----
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR,
+  cover_state VARCHAR,
+  genre_id INT NULL REFERENCES genres(id),
+  author_id INT NULL REFERENCES authors(id),
+  label_id INT NULL REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL
+);
 
+-- Create Labels tables
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(150) NOT NULL,
+  color VARCHAR(150) NOT NULL
+);
 
 
 
